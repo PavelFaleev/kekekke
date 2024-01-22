9e2acee80a0ec96176a67aed5aee7291459a935b
PavelFaleev <fraisley2003po@gmail.com>
2024-01-22 10:31:01 +0200

    Вывод результата

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        
+        string[] inputStrings = { "Hello", "2", "world", ":-)" };
+
+        // Формирование нового массива
+        string[] resultStrings = FilterArray(inputStrings);
+
+        // Вывод результата
+        Console.WriteLine("Результат: [" + string.Join(", ", resultStrings) + "]");
+    }
+

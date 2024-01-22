0c15f06924eed96d7f45b367c503f7b1a0bbd9c5
PavelFaleev <fraisley2003po@gmail.com>
2024-01-22 10:37:19 +0200

    улеснение примера

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 
 class Program
-{
+
     static void Main()
     {
         
-        
-        string[] inputStrings = { "Hello", "2", "world", ":-)" };
+        string[] inputStrings1 = { "Hello", "2", "world", ":-)" };
+        string[] resultStrings1 = FilterArray(inputStrings1);
+        Console.WriteLine("Результат 1: [" + string.Join(", ", resultStrings1) + "]");
 
-        // Формирование нового массива
-        string[] resultStrings = FilterArray(inputStrings);
+        string[] inputStrings2 = { "1234", "1567", "-2", "computer science" };
+        string[] resultStrings2 = FilterArray(inputStrings2);
+        Console.WriteLine("Результат 2: [" + string.Join(", ", resultStrings2) + "]");
 
-        // Вывод результата
-        Console.WriteLine("Результат: [" + string.Join(", ", resultStrings) + "]");
+        string[] inputStrings3 = { "Russia", "Denmark", "Kazan" };
+        string[] resultStrings3 = FilterArray(inputStrings3);
+        Console.WriteLine("Результат 3: [" + string.Join(", ", resultStrings3) + "]");
     }
-
